1ee5ac170afa0950bc7241b6a0ae7b442f5f95c3
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-17 02:50:11 +0300

    add capturefile, uploadfile and main table

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, {Component} from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
 import Web3 from 'web3';
 import './App.css';
 
-//Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({host: 'ipfs.infura.io', port: 5001, protocol: 'https'}) // leaving out the arguments will default to these values
 
 class App extends Component {
 
@@ -26,53 +27,92 @@ class App extends Component {
     }
 
     async loadBlockchainData() {
-        //Declare Web3
-        const web3 = window.web3;
-        console.log(web3);
-
-        //Load account
-
-        //Network ID
-
-        //IF got connection, get data from contracts
-        //Assign contract
-
-        //Get files amount
-
-        //Load files&sort by the newest
-
-        //Else
-        //alert Error
-
+        const web3 = window.web3
+        // Load account
+        const accounts = await web3.eth.getAccounts()
+        this.setState({account: accounts[0]})
+        // Network ID
+        const networkId = await web3.eth.net.getId()
+        const networkData = DStorage.networks[networkId]
+        if (networkData) {
+            // Assign contract
+            const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+            this.setState({dstorage})
+            // Get files amount
+            const filesCount = await dstorage.methods.fileCount().call()
+            this.setState({filesCount})
+            // Load files&sort by the newest
+            for (var i = filesCount; i >= 1; i--) {
+                const file = await dstorage.methods.files(i).call()
+                this.setState({
+                    files: [...this.state.files, file]
+                })
+            }
+        } else {
+            window.alert('DStorage contract not deployed to detected network.')
+        }
     }
 
     // Get file from user
     captureFile = event => {
+        event.preventDefault()
+
+        const file = event.target.files[0]
+        const reader = new window.FileReader()
+
+        reader.readAsArrayBuffer(file)
+        reader.onloadend = () => {
+            this.setState({
+                buffer: Buffer(reader.result),
+                type: file.type,
+                name: file.name
+            })
+            console.log('buffer', this.state.buffer)
+        }
     }
 
-
-    //Upload File
     uploadFile = description => {
+        console.log("Submitting file to IPFS...")
 
-        //Add file to the IPFS
+        // Add file to the IPFS
+        ipfs.add(this.state.buffer, (error, result) => {
+            console.log('IPFS result', result.size)
+            if (error) {
+                console.error(error)
+                return
+            }
 
-        //Check If error
-        //Return error
-
-        //Set state to loading
-
-        //Assign value for the file without extension
-
-        //Call smart contract uploadFile function
-
+            this.setState({loading: true})
+            // Assign value for the file without extension
+            if (this.state.type === '') {
+                this.setState({type: 'none'})
+            }
+            this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({from: this.state.account}).on('transactionHash', (hash) => {
+                this.setState({
+                    loading: false,
+                    type: null,
+                    name: null
+                })
+                window.location.reload()
+            }).on('error', (e) => {
+                window.alert('Error')
+                this.setState({loading: false})
+            })
+        })
     }
 
-    //Set states
     constructor(props) {
         super(props)
-        this.state = {}
-
-        //Bind functions
+        this.state = {
+            account: '',
+            dstorage: null,
+            files: [],
+            loading: false,
+            type: null,
+            name: null
+        }
+        this.uploadFile = this.uploadFile.bind(this)
+        this.captureFile = this.captureFile.bind(this)
     }
 
     render() {
