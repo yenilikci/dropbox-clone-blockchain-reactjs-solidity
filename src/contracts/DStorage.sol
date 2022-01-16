1a10ee973b17297e72b1253bd9bda745e2b18138
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-17 00:36:36 +0300

    add uploadFile func, File struct, FileUploaded event

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -2,36 +2,57 @@ pragma solidity ^0.5.0;
 
 contract DStorage {
   string public name = 'DStorage';
-  // Number of files
-  // Mapping fileId=>Struct
-
+  uint public fileCount = 0;
+  mapping(uint => File) public files;
   // Struct
-
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable upload;
+  }
 
   // Event
+  event FileUploaded(
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
 
   constructor() public {
   }
 
   // Upload File function
 
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
     // Make sure the file hash exists
-
+    require(bytes(_fileHash).length > 0);
     // Make sure file type exists
-
+    require(bytes(_fileType).length > 0);
     // Make sure file description exists
-
+    require(bytes(_fileDescription).length > 0);
     // Make sure file fileName exists
-
+    require(bytes(_fileName).length > 0);
     // Make sure uploader address exists
-
+    require(msg.sender != address(0));
     // Make sure file size is more than 0
-
-
+    require(_fileSize>0);
     // Increment file id
+    fileCount ++;
 
     // Add File to the contract
+    files[fileCount] = File(fileCount, _fileHash,  _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
 
     // Trigger an event
-
+    emit FileUploaded(fileCount, _fileHash,  _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+  }
 }
